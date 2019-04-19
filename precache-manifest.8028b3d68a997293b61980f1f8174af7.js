3320eca88b43918874337defe3eef021e2639f2c
Vaibhav <vaibhpraky@gmail.com>
2019-04-19 20:47:38 +0530

    Updates

---
+++ precache-manifest.8028b3d68a997293b61980f1f8174af7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e1f4d08b630e9d0728b2f0a5292ef1f",
+    "url": "/tsn/static/media/write.5e1f4d08.jpg"
+  },
+  {
+    "revision": "fbae83d11aae6ba55161e7745456b667",
+    "url": "/tsn/static/media/welcome.fbae83d1.jpg"
+  },
+  {
+    "revision": "f9aa83f89d6c954061622531a1d9a014",
+    "url": "/tsn/static/media/profile.f9aa83f8.svg"
+  },
+  {
+    "revision": "3b7df07c713d7b349484",
+    "url": "/tsn/static/js/runtime~main.cd961c95.js"
+  },
+  {
+    "revision": "72463f08472cd58e52a0",
+    "url": "/tsn/static/js/main.a2f5a9ae.chunk.js"
+  },
+  {
+    "revision": "5793215efe595d91e82d",
+    "url": "/tsn/static/js/2.c98741cf.chunk.js"
+  },
+  {
+    "revision": "72463f08472cd58e52a0",
+    "url": "/tsn/static/css/main.ede4e9c0.chunk.css"
+  },
+  {
+    "revision": "5793215efe595d91e82d",
+    "url": "/tsn/static/css/2.c9dd762e.chunk.css"
+  },
+  {
+    "revision": "ec6c7ed613436f171ad6ce8f9c5858e7",
+    "url": "/tsn/index.html"
+  }
+];
--- src/apis/server.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  // baseURL: "http://192.168.43.75:3000/api"
-  baseURL: "https://agile-brook-48457.herokuapp.com/api"
-});
